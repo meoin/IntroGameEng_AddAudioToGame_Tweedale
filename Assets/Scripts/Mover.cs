941f658fe2e5eb99de5879242d040138be31b977
Michael <hubu99@gmail.com>
2025-11-08 16:15:09 -0400

    Initial commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+
+
+    private void Start()
+    {
+        rb =  GetComponent<Rigidbody>();
+        rb.linearVelocity = transform.forward * speed;
+    }
+}
